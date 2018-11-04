7dbd283342650377dbb17697ed7ca91e1bb301ad
Can Web Design <mrthack@hotmail.com>
2018-11-04 12:17:44 +0300

    sdfsadf
    sfads

---
+++ precache-manifest.08a2a544c6b5696e1fd3709903882644.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "61a536b0d04a0d782378d23717273773",
+    "url": "/static/media/logo.61a536b0.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "214a750e31e16da9fbef",
+    "url": "/static/js/main.214a750e.chunk.js"
+  },
+  {
+    "revision": "4d1655f222927aa0ab5b",
+    "url": "/static/js/1.4d1655f2.chunk.js"
+  },
+  {
+    "revision": "214a750e31e16da9fbef",
+    "url": "/static/css/main.207909dc.chunk.css"
+  },
+  {
+    "revision": "27de077669ef70b64853a0f3d835119b",
+    "url": "/index.html"
+  }
+];
